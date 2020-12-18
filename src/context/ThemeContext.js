d0bbc07d3e60f9ea2456dc4fda82112710363505
tplane-js <jsdev.remote@gmail.com>
2020-12-17 22:04:46 -0500

    optimized render

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,20 +1,24 @@
 import React, { useState, createContext, useLayoutEffect } from "react";
 import { useEffect } from "react";
 
+const localStorageKey = 'DARK_THEME_KEY';
+
 const ThemeContext = createContext({
   dark: false,
   toggleTheme: () => {},
 });
 
 
-const stringToBool = str => JSON.parse(str.toLowerCase());
+const stringToBool = str => {
+  return str === null ? false : JSON.parse(str);
+}
   
 export const ThemeProvider = (props) => {
-  const [dark, setDark] = useState(stringToBool(window.localStorage.getItem("darkTheme")));
-  
+  const [dark, setDark] = useState(stringToBool(window.localStorage.getItem(localStorageKey)));
+
   const toggleTheme = () => {
     setDark(!dark);
-    window.localStorage.setItem("darkTheme", !dark);
+    window.localStorage.setItem(localStorageKey, !dark);
   };
 
   return (
